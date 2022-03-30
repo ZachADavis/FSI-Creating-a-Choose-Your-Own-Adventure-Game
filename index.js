626f97238438383d210239e5fb3368cdd93c1949
Zachary Davis <kicksilver@icloud.com>
2022-03-30 18:25:47 -0400

    Adventure updates

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Your Code here
+let answer = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left') {
+   let didFollow = window.prompt("You come across a stray cat. It scampers off down a small hole just large enough for you to crawl through. Do you follow it or continue on your path?")
+   if(didFollow === 'follow'){
+      let catKing = window.prompt("The small feline leads you down the hole as a doctor guiding the birth of a new king. Suddenly, you find yourself surrounded by kittens. You have been crowned cat King and live your days in prosperiety.")
+      
+   }
+
+}
+else(/*The bad loser path of right)*/ {
+    
+}
